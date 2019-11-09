d8cfc275ec6c1b3f63e01b9545a6fe3cfe680351
AsornXD <45113995+AsornXD@users.noreply.github.com>
2019-11-09 12:53:08 -0500

    Update beemovie.java

--- src/app/beemovie.java
+++ src/app/beemovie.java
@@ -17,7 +17,7 @@ class beemovie {
                 for (int i = 0; i < curstr.length(); i++) {
                     System.out.println(curstr);
                     if (curstr.charAt(i) == 'B' && curstr.charAt(i + 1) == 'e' && curstr.charAt(i + 2) == 'e') {
-                        recurses = new beemovie("C:\\Users\\Flame\\IdeaProjects\\test 1\\Chapter 2\\thack\\Terrible-hack\\src\\app\\Bee movie script.txt");
+                        recurses = new beemovie(System.getProperty("user.dir")+"\\src\\app\\Bee movie script.txt");
                         recurses.recurse();
                     }
                 }
