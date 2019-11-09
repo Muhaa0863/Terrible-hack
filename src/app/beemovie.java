0468adb5c6a978f68c12d69abd5d0d5248742018
AsornXD <45113995+AsornXD@users.noreply.github.com>
2019-11-09 12:36:09 -0500

    bee movie recurse thingy lol

--- src/app/beemovie.java
+++ src/app/beemovie.java
@@ -1,22 +1,30 @@
 package app;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 
-public class beemovie {
-    public FileReader fileReader;
-    public beemovie() throws Exception{
-        fileReader = new FileReader("Bee movie script.txt");
+class beemovie {
+    private FileReader fileReader;
+    public beemovie(String str) throws Exception{
+        fileReader = new FileReader(str);
     }
     public void recurse () throws Exception{
         BufferedReader reader = new BufferedReader(fileReader);
-        String curstr;
-        while (true){
-            curstr = reader.readLine();
-            
+        String curstr = "";
+        beemovie recurses;
+        try {
+            while (reader.ready()) {
+                curstr = reader.readLine();
+                for (int i = 0; i < curstr.length(); i++) {
+                    System.out.println(curstr);
+                    if (curstr.charAt(i) == 'B' && curstr.charAt(i + 1) == 'e' && curstr.charAt(i + 2) == 'e') {
+                        recurses = new beemovie("C:\\Users\\Flame\\IdeaProjects\\test 1\\Chapter 2\\thack\\Terrible-hack\\src\\app\\Bee movie script.txt");
+                        recurses.recurse();
+                    }
+                }
+            }
         }
-
+        catch (IOException e){
+            System.out.println("broken");
+        }
     }
 }
--- src/app/test.java
+++ src/app/test.java
@@ -1,7 +1,8 @@
 package app;
 
 public class test {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        beemovie cur = new beemovie("C:\\Users\\Flame\\IdeaProjects\\test 1\\Chapter 2\\thack\\Terrible-hack\\src\\app\\Bee movie script.txt");
+        cur.recurse();
     }
 }
