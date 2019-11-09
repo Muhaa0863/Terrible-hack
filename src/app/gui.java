245c92ec4ccd40c605c11b6fcf0447c4b8a7f71e
AsornXD <45113995+AsornXD@users.noreply.github.com>
2019-11-09 15:58:06 -0500

    bruh click sound effects

--- src/app/gui.java
+++ src/app/gui.java
@@ -2,34 +2,53 @@ package app;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 public class gui{
-
-  public static void main(String[] args) {
-
-    JFrame f = new JFrame("A JFrame");
-    f.setSize(250, 250);
-    f.setLocation(300,200);
-    final JTextArea textArea = new JTextArea(10, 40);
-    f.getContentPane().add(BorderLayout.CENTER, textArea);
-    final JButton button = new JButton("Click Me");
-    f.getContentPane().add(BorderLayout.SOUTH, button);
-    long start = System.currentTimeMillis();
-    button.addActionListener(new ActionListener() {
-        int c = 0;
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            c++;
-            long end = System.currentTimeMillis();
-            float sec = (end - start) / 1000F;
-            textArea.setText("Clicks per second: " + c/sec);
-        }
+private static Clip clip;
+private static AudioInputStream audioInputStream;
+  public static void main(String[] args) throws Exception{
+      audioInputStream = AudioSystem.getAudioInputStream(new File(System.getProperty("user.dir")+"\\src\\app\\Bruh Sound Effect 2.wav").getAbsoluteFile());
+      JFrame f = new JFrame("bruh clicks");
+      f.setSize(250, 250);
+      f.setLocation(300,200);
+      final JTextArea textArea = new JTextArea(10, 40);
+      f.getContentPane().add(BorderLayout.CENTER, textArea);
+      final JButton button = new JButton("Click Me");
+      clip = AudioSystem.getClip();
+      clip.open(audioInputStream);
+      f.getContentPane().add(BorderLayout.SOUTH, button);
+      long start = System.currentTimeMillis();
+      button.addActionListener(new ActionListener() {
+          int c = 0;
+          @Override
+          public void actionPerformed(ActionEvent e){
+              c++;
+              long end = System.currentTimeMillis();
+              float sec = (end - start) / 1000F;
+              textArea.setText("Clicks per second: " + c / sec);
+              clip.start();
+              try {
+                  audioInputStream = AudioSystem.getAudioInputStream(new File(System.getProperty("user.dir") + "\\src\\app\\Bruh Sound Effect 2.wav").getAbsoluteFile());
+                  clip = AudioSystem.getClip();
+                  clip.open(audioInputStream);
+              }
+              catch (IOException d){
+                  System.out.println("Broken");
+              }
+              catch (UnsupportedAudioFileException f){
+                  System.out.println("Brokens");
+              }
+              catch (LineUnavailableException g){
+                  System.out.println("Brokenss");
+              }
+          }
     });
-
-    f.setVisible(true);
+      f.setVisible(true);
 
   }
 
